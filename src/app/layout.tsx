ce77943ce8f6632384d8081ef28e5f7a9b2199f5
Buska909 <alexnclas@gmail.com>
2025-01-15 01:07:38 +0100

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-white`}
       >
